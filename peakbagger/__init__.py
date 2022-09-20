3ebc1243e321e6b5c8b8d73330f4239d8ab79526
Jacob Ferrante <jacobferrante@protonmail.com>
2022-09-20 17:12:29 -0400

    fixed structure, worked on update feature

--- peakbagger/__init__.py
+++ peakbagger/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,3 +9,4 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sqlite'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
 db = SQLAlchemy(app)
 
+from peakbagger import routes
--- peakbagger/routes.py
+++ peakbagger/routes.py
@@ -1,6 +1,7 @@
+from crypt import methods
 from flask import render_template, redirect
 from peakbagger import app, db
-from peakbagger.forms import CreateHike
+from peakbagger.forms import CreateHike, UpdateHike
 from peakbagger.models import Hike
 
 ## Home Route
@@ -22,7 +23,7 @@ def hike():
     return render_template('create.html', form=form)
 
 ## Delete the hike
-@app.route("/hike/delete/<int:hike_id>")
+@app.route("/hike/delete/<int:hike_id>", methods=['POST'])
 def delete(hike_id):
     hike = Hike.query.filter_by(id=hike_id).first()
     db.session.delete(hike)
@@ -30,13 +31,14 @@ def delete(hike_id):
     return redirect('/')
 
 # Update the hike
-@app.route("/hike/update/<int:hike_id>")
-def delete(hike_id):
+@app.route("/hike/update/<int:hike_id>", methods=['GET, POST'])
+def update(hike_id):
     hike = Hike.query.filter_by(id=hike_id).first()
-    form = CreateHike()
-    if form.validate_on_submit():
-        post = Hike(name=form.name.data, notes=form.notes.data, link=form.link.data)
-        db.session.add(post)
+    form = UpdateHike()
+    if form.validate_on_submit(): 
+        hike.name = form.name.data 
+        hike.notes = form.notes.data
+        hike.link = form.link.data
         db.session.commit()
         return redirect('/')
     return render_template('create.html', form=form)
