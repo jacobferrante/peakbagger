4c9890dd07e8d1eb655555d4ae16add7ef85e268
Jacob Ferrante <jacobferrante@protonmail.com>
2022-09-26 13:38:02 -0400

    rewrite with fetch api and cleaning up, lots more to do

--- peakbagger/routes.py
+++ peakbagger/routes.py
@@ -4,15 +4,19 @@ from peakbagger.forms import CreateHike
 from peakbagger.models import Hike
 
 ## Home Route
-@app.route("/", methods=["GET"])
-@app.route ("/hike", methods=["GET"])
+@app.get("/")
 def home():
     hikes = Hike.query.all()
     return render_template("home.html", hikes=hikes)
 
-# Create hikes using request form
-@app.route("/hike/create", methods=['GET', 'POST'])
+@app.get('/hike')
 def hike():
+    form = CreateHike()
+    return render_template('create.html', form=form)
+
+# Create hikes using request form
+@app.post("/hike")
+def new_hike():
     form = CreateHike()
     if form.validate_on_submit():
         post = Hike(name=form.name.data, notes=form.notes.data, link=form.link.data)
@@ -22,28 +26,30 @@ def hike():
     return render_template('create.html', form=form)
 
 # Update the hike
-@app.route("/hike/update/<int:hike_id>", methods=['GET', 'POST'])
-def update(hike_id):
-    hike = Hike.query.filter_by(id=hike_id).first()
-    form = CreateHike()
-    ## Ask for user input to update hike
-    if form.validate_on_submit():
-        hike.name = form.name.data
-        hike.notes = form.notes.data
-        hike.link = form.link.data
-        db.session.commit()
-        return redirect('/')
-    ## Populate form with the current database info for that item
-    elif request.method == 'GET':
-        form.name.data = hike.name
-        form.notes.data = hike.notes
-        form.link.data = hike.link
-    return render_template('create.html', form=form)
 
 ## Delete the hike
-@app.route("/hike/delete/<int:hike_id>", methods=['GET', 'POST'])
+@app.delete("/hike/<int:hike_id>")
 def delete(hike_id):
     hike = Hike.query.filter_by(id=hike_id).first()
     db.session.delete(hike)
     db.session.commit()
     return redirect('/')
+
+
+#@app.put("/hike/<int:hike_id>") 
+#def update(hike_id):
+#    hike = Hike.query.filter_by(id=hike_id).first()
+#    form = CreateHike()
+#    ## Ask for user input to update hike
+#    if form.validate_on_submit():
+#        hike.name = form.name.data
+#        hike.notes = form.notes.data
+#        hike.link = form.link.data
+#        db.session.commit()
+#        return redirect('/')
+#    ## Populate form with the current database info for that item
+#    elif request.method == 'GET':
+#        form.name.data = hike.name
+#        form.notes.data = hike.notes
+#        form.link.data = hike.link
+#    return render_template('create.html', form=form)
